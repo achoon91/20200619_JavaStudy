d29c365b2c0ee7812aa0f4b3943a222a40b8d6a4
SIST <SIST@DESKTOP-BDRG0TG>
2020-06-19 14:56:33 +0900

    first

--- 20200619-깃허브/src/MainClass.java
+++ 20200619-깃허브/src/MainClass.java
@@ -7,6 +7,8 @@ public class MainClass {
 		System.out.println("Hello Java!!");
 		System.out.println("Hello Java!!");
 		System.out.println("Hello Java!!");
+		System.out.println("Hello Java!!");
+		
 		
 
 	}
