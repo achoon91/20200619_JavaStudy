dbb516be4a2130c61da13ee595aa4ea4fb95f4d2
Chihoon <achoon91@gmail.com>
2020-12-29 14:24:16 +0900

    자바제어문(다중조건문)

---
+++ 20200624-자바제어문(다중조건문)/src/다중조건문4.java
@@ -0,0 +1,27 @@
+/*
+ *   사용자 => 정수입력 (1~12)
+ *   12,1,2 => 겨울입니다
+ *   3,4,5  => 봄입니다
+ *   if(month==3 || month==4 || month==5)
+ *   if(month>=3 && month<=5)
+ *   6,7,8  => 여름입니다
+ *   9,10,11 => 가을입니다
+ *   
+ *   ====================================
+ *   ==> 문자 
+ *       ch=scan.next().charAt(0)
+ *   문자를 입력을 받아서 char ch 
+ *     = 숫자    ch>='0' && ch<='9'
+ *     = 알파벳 ch>='A' && ch<='Z' || ch>='a' && ch<='z'
+ *     = 한글    ch>='가' && ch<='R'
+ *     = 특수문자 
+ *   
+ */
+public class 다중조건문4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 20200624-자바제어문(다중조건문)/src/선택문.java
@@ -0,0 +1,75 @@
+/*
+ *   선택문 
+ *    => switch(정수,문자,문자열)
+ *       {
+ *          // 정수
+ *          case 정수값:  => :
+ *             처리문장 
+ *             ==> if(조건){처리}
+ *          case 정수값:
+ *             처리문장   ===> 중복된 값은 사용할 수 없다 
+ *          
+ *       }
+ *       
+ *       switch(a)
+ *       {
+ *          case 1:
+ *               == 라벨 (중복이 없다)
+ *          case 1:  ==> error 
+ *          
+ *          if(a==1){System.out.println(1);} 
+ *          if(a==1){System.out.println("일");}
+ *          
+ *          if(a==1)
+ *          {
+ *             System.out.println(1);
+ *             System.out.println("일");
+ *          }
+ *       }
+ *       
+ *       // 사용자 => 정수=1
+ *       switch(정수)  ==> break가 없는 경우에는 모든 문장을 수행 
+ *       {
+ *          case 1:
+ *             처리문장 1
+ *          case 2:
+ *             처리문장 2
+ *             break;
+ *          case 3:
+ *             처리문장 3 
+ *       }
+ */
+import java.util.*;
+public class 선택문 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int select=0;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("정수(1~5):");
+        select=scan.nextInt();
+        // if(select==1 || select==2)
+        // 종료 시점 ==> break;
+        switch(select)
+        {
+        case 1:
+        	//System.out.println("1번~2번을 입력하였습니다");
+        	//break;// switch문 종료한다 
+        case 2:
+        	System.out.println("1번~2번을 입력하였습니다");
+        	break;
+        case 3:
+        	System.out.println("3번을 입력하였습니다");
+        	break;
+        case 4:
+        	System.out.println("4번을 입력하였습니다");
+        	break;
+        case 5:
+        	System.out.println("5번을 입력하였습니다");
+        	break;
+        default:
+        	System.out.println("1~5번까지만 사용하세요");
+        }
+	}
+
+}
