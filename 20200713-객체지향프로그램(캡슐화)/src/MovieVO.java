6ce819c75621648ca58f4fb826e9f133459e9c7c
Chihoon <achoon91@gmail.com>
2021-01-20 11:20:28 +0900

    객체지향프로그램(캡슐화)

---
+++ 20200713-객체지향프로그램(캡슐화)/src/MovieVO.java
@@ -0,0 +1,16 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MovieVO {
+   private int mno;
+   private String title;
+   private String director;
+   private String actor;
+   private String genre;
+   private String grade;
+   private double score;
+   private String story;
+   
+}
