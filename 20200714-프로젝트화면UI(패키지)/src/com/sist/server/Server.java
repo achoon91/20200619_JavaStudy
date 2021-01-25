e07a4b5dd43aebebefa66b17f79c5aa4ad77bf96
Chihoon <achoon91@gmail.com>
2021-01-25 22:53:29 +0900

    프로젝트화면UI(패키지)

---
+++ 20200714-프로젝트화면UI(패키지)/src/com/sist/server/Server.java
@@ -0,0 +1,10 @@
+package com.sist.server;
+import com.sist.common.*;
+import java.net.*;
+public class Server {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
