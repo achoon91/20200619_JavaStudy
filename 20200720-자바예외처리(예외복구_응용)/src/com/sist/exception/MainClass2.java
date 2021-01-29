4284910f2aed665e623210b58e3d459d683d4a26
Chihoon <achoon91@gmail.com>
2021-01-29 22:48:06 +0900

    자바예외처리(예외복구_응용)

---
+++ 20200720-자바예외처리(예외복구_응용)/src/com/sist/exception/MainClass2.java
@@ -0,0 +1,36 @@
+package com.sist.exception;
+
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        try
+        {
+        	
+        }catch(ArithmeticException e)
+        {
+        	
+        }catch(ArrayIndexOutOfBoundsException e)
+        {
+        	
+        }catch(NumberFormatException e)// web¿¡¼­ ÁÖ·Î Ãâ·ÂÇÏ´Â error
+        {
+        	
+        }
+        
+        try
+        {
+        	
+        }catch(RuntimeException e) {}
+        
+        try
+        {
+        	
+        }catch(ArithmeticException | ArrayIndexOutOfBoundsException | 
+        		NumberFormatException e)
+        {
+        	
+        }
+	}
+
+}
